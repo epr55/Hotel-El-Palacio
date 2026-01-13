2bb0569b4af142c7b9d07876130de325266ec20c
agh45-ua <agh45@alu.ua.es>
2026-01-13 12:24:00 +0100

    añadidos seeders categorias y servicios y demas actualizados

--- Hotel-El-Palacio/database/seeders/ServicioSeeder.php
+++ Hotel-El-Palacio/database/seeders/ServicioSeeder.php
@@ -9,6 +9,41 @@ class ServicioSeeder extends Seeder
 {
     public function run(): void
     {
-        Servicio::factory()->count(8)->create();
+        $servicios = [
+            [
+                'nombre' => 'Desayuno buffet',
+                'descripcion' => '/pers./día',
+                'precio' => 15,
+                'tipo_cobro' => 'por_persona_noche',
+            ],
+            [
+                'nombre' => 'Parking privado',
+                'descripcion' => '/día',
+                'precio' => 10,
+                'tipo_cobro' => 'por_noche',
+            ],
+            [
+                'nombre' => 'Acceso al Spa',
+                'descripcion' => '/sesión',
+                'precio' => 25,
+                'tipo_cobro' => 'personalizable_por_persona',
+            ],
+            [
+                'nombre' => 'Traslado al aeropuerto',
+                'descripcion' => '/total (ida y vuelta)',
+                'precio' => 70,
+                'tipo_cobro' => 'unico',
+            ],
+            [
+                'nombre' => 'Late check-out',
+                'descripcion' => '/reserva',
+                'precio' => 20,
+                'tipo_cobro' => 'unico',
+            ],
+        ];
+
+        foreach ($servicios as $servicio) {
+            Servicio::create($servicio);
+        }
     }
 }
